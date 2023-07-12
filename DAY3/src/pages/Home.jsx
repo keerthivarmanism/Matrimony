6333b4f3fc461cc316b0bd865ab117d5066101ca
keerthivarmanism <119048367+keerthivarmanism@users.noreply.github.com>
2023-07-12 15:41:39 +0530

    Add files via upload

---
+++ DAY3/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { Link } from "react-router-dom";
+import SideBar from "../components/Sidebar";
+
+function Login() {
+    return (
+        <SideBar/>
+    );
+}
+return SideBar
